e26bfc387080c487ad1460025a2de25c76d47726
Michal Srb <msrb@redhat.com>
2017-03-18 22:08:25 +0100

    test

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -21,14 +21,21 @@ def call(body) {
     sh "mvn org.codehaus.mojo:versions-maven-plugin:2.2:set -U -DnewVersion=${config.version}"
     sh "mvn clean -e -U deploy -Dmaven.test.skip=${skipTests} ${profile}"
 
-    if (flow.hasService("bayesian")) {
+    if (flow.hasService("bayesian-link")) {
         try {
-          sh 'mvn io.github.stackinfo:stackinfo-maven-plugin:0.1:prepare'
-          def response = bayesianAnalysis()
+            sh 'mvn io.github.stackinfo:stackinfo-maven-plugin:0.2:prepare'
+            sh 'ping -c 1 bayesian-link'
+            bayesianUrl = flow.getServiceURL('bayesian-link') //'http://api-bayesian.dev.rdu2c.fabric8.io/'
+            def response = bayesianAnalysis url: bayesianUrl
+            if (response.success) {
+                def utils = new io.fabric8.Utils()
+                def buildName = utils.getValidOpenShiftBuildName()
+                utils.addAnnotationToBuild(buildName, 'fabric8.io/bayesian.analysisUrl', response.getAnalysisUrl())
+            }
         } catch (err) {
-          echo "Unable to run Bayesian analysis: ${err}"
+            echo "Unable to run Bayesian analysis: ${err}"
         }
-    }
+	}
 
     //try sonarQube
     sonarQubeScanner(body);
