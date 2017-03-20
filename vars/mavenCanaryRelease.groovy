cb9ee3bddc6e51ce72a596d84cfd5ad7cddfe461
Michal Srb <msrb@redhat.com>
2017-03-20 10:07:44 +0100

    curl the b link

--- vars/mavenCanaryRelease.groovy
+++ vars/mavenCanaryRelease.groovy
@@ -25,7 +25,7 @@ def call(body) {
         try {
             sh 'mvn io.github.stackinfo:stackinfo-maven-plugin:0.2:prepare'
             //bayesianUrl = flow.getServiceURL('bayesian-link') //'http://api-bayesian.dev.rdu2c.fabric8.io/'
-            sh 'nslookup bayesian-link'
+            sh 'curl -v -i http://bayesian-link:80'
             def response = bayesianAnalysis url: 'http://bayesian-link:80'
             if (response.success) {
                 def utils = new io.fabric8.Utils()
